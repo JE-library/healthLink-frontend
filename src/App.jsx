a900b33a146982f416acd67e6d9b8f6ac3b20b9e
Josephine <jossyasiedu96@gmail.com>
2025-07-09 14:32:38 +0000

    startup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+  <nav>
+    <span>LOGO</span>
+    <ul>
+      <li></li>
+    </ul>
+  </nav>
+  )
+}
+
+export default App
---
+++ src/layouts/patientsdashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const patientsdashboard = () => {
+  return (
+    <div>patientsdashboard</div>
+  )
+}
+
+export default patientsdashboard
